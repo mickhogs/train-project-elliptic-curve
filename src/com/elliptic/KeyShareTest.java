5ae36d4997fef99c4a12419ccaca9d8831072af8
mickhogs <mick@nous.net>
2019-07-24 23:30:48 +0100

    test class with simple key

---
+++ src/com/elliptic/KeyShareTest.java
@@ -0,0 +1,75 @@
+package com.elliptic;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import com.elliptic.curve.EllipticCurve;
+import com.elliptic.point.Point;
+
+public class KeyShareTest {
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		
+		EllipticCurve btcCurve = EllipticCurve.btcEllipticCurve();
+		
+		// Key shares - key is 1000
+		final BigInteger aliceShare = new BigInteger("800");
+		final BigInteger bobShare = new BigInteger("100");
+		final BigInteger jackShare = new BigInteger("100");
+		
+		// private key assembled from Alice, Bob and Jack's shares
+		BigInteger privateKey = aliceShare.add(bobShare).add(jackShare);
+		System.out.println("Private key: " + privateKey);
+		
+		//  public key
+		Point publicKey = btcCurve.multiplyWithBasePoint(privateKey);
+		System.out.println("Public key: " + publicKey);
+		
+		// Create hash of 'transaction'
+		final String text = "This is a bitcoin transaction: Alice pay Bob 10 BTC";
+		final MessageDigest md = MessageDigest.getInstance("SHA1");
+		md.update(text.getBytes());
+		final byte[] hashByte = md.digest();
+		final BigInteger hash = new BigInteger(hashByte).abs();
+		System.out.println("Text: " + text + ", hash: " + hash);
+		
+		// a random key is generated for each signature usually - we will just use this for testing
+		final BigInteger randomKey = new BigInteger("184956791234683476421138297853832852980077394385708898754696965181764999991212");
+		final Point randomPoint = btcCurve.multiplyWithBasePoint(randomKey);
+		System.out.println("Random point: " + randomPoint);
+		
+		// r - > cord x of random point mod order. This is the first half of the signature. We generate a term 's' which is verified with public key against the term 'r'
+		final BigInteger r = randomPoint.getxCord().remainder(btcCurve.getOrder());
+		
+		// S computed form Bob, Alice and Jack's share of the key
+		final BigInteger aliceHash = hash.add(r.multiply(aliceShare));
+		final BigInteger bobHash = aliceHash.add(r.multiply(bobShare));
+		final BigInteger jackhash = bobHash.add(r.multiply(jackShare));
+		
+		// calculate s - need that random key again..
+		final BigInteger s = jackhash.multiply(randomKey.modInverse(btcCurve.getOrder())).remainder(btcCurve.getOrder());
+		
+		System.out.println("(r,s) " + "(" + r + ", " + s + ")");
+		// verify the signature:
+		final BigInteger w = s.modInverse(btcCurve.getOrder());
+						
+		final Point u1 = btcCurve.multiplyWithBasePoint((hash.multiply(w).remainder(btcCurve.getOrder())));
+		final Point u2 = btcCurve.pointMultiplication(publicKey, (r.multiply(w).remainder(btcCurve.getOrder())));
+		final Point checkpoint = btcCurve.pointAddition(u1, u2);
+				
+		if(checkpoint.getxCord().compareTo(r) == 0){
+					
+			System.out.println("signature is valid...");
+					
+		} else {
+					
+			System.out.println("invalid signature detected!!!");
+					
+		}
+		// for comparison - create signature with private key for comparison against Alice, Bob and Jack
+		final BigInteger sFromPrivKey = (hash.add(r.multiply(privateKey)).multiply(randomKey.modInverse(btcCurve.getOrder()))).remainder(btcCurve.getOrder());
+		System.out.println("(r,sPriv) " + "(" + r + ", " + sFromPrivKey + ")");
+	}
+	
+}
--- src/com/elliptic/curve/EllipticCurve.java
+++ src/com/elliptic/curve/EllipticCurve.java
@@ -2,17 +2,122 @@ package com.elliptic.curve;
 
 import java.math.BigInteger;
 
+import com.elliptic.point.Point;
+
 public class EllipticCurve {
 
 	private final BigInteger mod;
 	private final BigInteger order;
 	private final BigInteger a;
 	private final BigInteger b;
+	private final Point basePoint; // usualy termed 'G'
 	
-	public EllipticCurve(BigInteger mod, BigInteger order, BigInteger a, BigInteger b) {
+	public EllipticCurve(BigInteger mod, BigInteger order, BigInteger a, BigInteger b, Point basePoint) {
 		this.mod = mod;
 		this.order = order;
 		this.a = a;
 		this.b = b;
+		this.basePoint = basePoint;
+	}
+	
+	public BigInteger getOrder() {
+		return order;
+	}
+	
+	private static BigInteger generatePrimeMod(){
+		///modulo for BTC: 2^256 - 2^32 - 2^9 - 2^8 - 2^7 - 2^6 - 2^4 - 2^0
+				
+		final BigInteger base = new BigInteger("2");
+				
+		return base.pow(256)
+			.subtract(base.pow(32))
+			.subtract(base.pow(9))
+			.subtract(base.pow(8))
+			.subtract(base.pow(7))
+			.subtract(base.pow(6))
+			.subtract(base.pow(4))
+			.subtract(base.pow(0));
+	}
+	
+	public Point multiplyWithBasePoint(BigInteger k) {
+		return pointMultiplication(basePoint, k);
+	}
+	
+	public Point pointMultiplication(Point p, BigInteger k) {
+		Point tempPoint = new Point(p.getxCord(), p.getyCord());
+		final String kAsBinary = k.toString(2);
+		
+		for(int i = 1; i < kAsBinary.length(); i++) {
+			int currentBit = Integer.parseInt(kAsBinary.substring(i, i+1));
+			tempPoint = pointAddition(tempPoint, tempPoint);
+			if(currentBit == 1) {
+				tempPoint = pointAddition(tempPoint, p);
+			}
+		}
+		return tempPoint;
+	}
+	
+	public Point pointAddition(Point P, Point Q) {
+		
+		
+		BigInteger x1 = P.getxCord();
+		BigInteger y1 = P.getyCord();
+		
+		BigInteger x2 = Q.getxCord();
+		BigInteger y2 = Q.getyCord();
+		
+		BigInteger beta;
+		
+		if(x1.compareTo(x2) == 0 && y1.compareTo(y2) == 0) {
+			
+			//apply doubling
+			
+			beta = (BigInteger.valueOf(3).multiply(x1.multiply(x1))
+					.add(a))
+					.multiply((BigInteger.valueOf(2).multiply(y1)).modInverse(mod));
+			
+		}
+		else {
+			
+			//apply point addition
+			
+			beta = (y2.subtract(y1))
+					.multiply(x2.subtract(x1).modInverse(mod));
+			
+		}
+		
+		BigInteger x3 = (beta.multiply(beta)).subtract(x1).subtract(x2);
+		BigInteger y3 = (beta.multiply(x1.subtract(x3))).subtract(y1);
+		
+		while(x3.compareTo(BigInteger.valueOf(0)) < 0) {
+			
+			BigInteger times = x3.abs().divide(mod).add(BigInteger.valueOf(1));
+			
+			x3 = x3.add(times.multiply(mod));
+			
+		}
+		
+		while(y3.compareTo(BigInteger.valueOf(0)) < 0) {
+			
+			BigInteger times = y3.abs().divide(mod).add(BigInteger.valueOf(1));
+			
+			y3 = y3.add(times.multiply(mod));
+			
+		}
+		
+		x3 =x3.remainder(mod);
+		y3 = y3.remainder(mod);
+				
+		return new Point(x3, y3);
+		
+	}
+	
+	public static EllipticCurve btcEllipticCurve() {
+		final BigInteger mod = generatePrimeMod();
+		final BigInteger order = new BigInteger("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16);
+		final BigInteger a = new BigInteger("0"), b = new BigInteger("7");
+		final Point basePointG = new Point(new BigInteger("55066263022277343669578718895168534326250603453777594175500187360389116729240"), 
+				new BigInteger("32670510020758816978083085130507043184471273380659243275938904335757337482424"));
+		return new EllipticCurve(mod, order,a, b, basePointG);
 	}
 }
